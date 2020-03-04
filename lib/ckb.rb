ed0cfe08ed24d1faf809fc5cc75f90f0c77734ad
quake <quake.wang@gmail.com>
2020-03-04 15:41:18 +0900

    cleanup

--- lib/ckb.rb
+++ lib/ckb.rb
@@ -22,23 +22,3 @@ module CKB
     @@network
   end
 end
-
-# rpc = CKB::RPC::new
-# tip_header = rpc.get_tip_header
-#
-# wallet = CKB::Wallet::new
-# tx = wallet.gen_tx("ckbxxx", "ckbxxx", 100)
-# signer = CKB::Signer::new("0x...private_key...")
-# signed_tx = signer.sign(tx)
-# rpc.send_transaction(signed_tx)
-
-
-
-# rpc = CKB::RPC.new("http://127.0.0.1:8114")
-# wallet = CKB::Wallet.new(rpc)
-# tx = wallet.gen_tx("ckbxxx1", "ckbxxx2", 100)
-# signed_tx = CKB::Signer.sign(tx, private_key)
-# rpc.send_transaction(signed_tx)
-
-
-# tx = wallet.gen_tx(["ckbxxx1", "ckbxxx2"], ["ckbxxx3": 100, "ckbxxx4": 200])
